ca51014da62e99a6f3d125d1b53a09587e275df7
Md. Ibrahim Khalilulla <ibrahimkhalilulla898@gmail.com>
2024-10-15 14:26:36 +0600

    Fourth commit

--- index.js
+++ index.js
@@ -3,36 +3,33 @@ const socketio = require("socket.io");
 const http = require("http");
 const path = require("path");
 
-
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-const port = 3000
+const port = 3000;
 
-app.set("view engine","ejs");
-app.set(express.static(path.join(__dirname,"public")));
-app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
+let markers = {};
 
-io.on("connection",function(socket){
-    console.log("Connected");
-    socket.on("send-location",function(data){
-      io.emit("receive-location",{
-        id : socket.id,
-        ...data,
-      });
+io.on("connection", (socket) => {
+    socket.emit("receive-location", markers);
+
+    socket.on("send-location", (data) => {
+        markers[socket.id] = { latitude: data.latitude, longitude: data.longitude };
+        io.emit("receive-location", markers);
     });
-    socket.on("disconnect",function(){
-      io.emit("user disconnected",socket.id);
+
+    socket.on("disconnect", () => {
+        delete markers[socket.id];
+        io.emit("user disconnected", socket.id);
     });
 });
 
+app.get('/', (req, res) => {
+    res.render("index");
+});
 
-
-app.get('/', function(req, res) {
-  res.render("index")
-})
-
-server.listen(port, () => {
-  console.log("Server Start")
-})
+server.listen(port);
